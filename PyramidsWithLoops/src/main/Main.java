dd691e9a7e4f80262760f5950e568d7efbcb9e97
thoma2aw <thoma2aw@LCBLAB2110-20.uccob.uc.edu>
2019-09-19 08:51:57 -0400

    added notes

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,15 +15,15 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 1; i < lines+1; i++) {
-			for(int k=0;k<lines-i;k++) {
-				System.out.print(" ");
+		int lines = 5;								//decides how many lines pyramid is
+		for (int i = 1; i < lines+1; i++) {			//outer loop to determine which line we are on and which number will be output
+			for(int k=0;k<lines-i;k++) {			
+				System.out.print(" ");				//inner loop to output spaces for proper spacing
 			}
-			for (int j=1;j<=i*2-1;j++) {		
+			for (int j=1;j<=i*2-1;j++) {			//inner loop to spit out a certain number of the character
 				System.out.print(""+i);	
 			}
-			System.out.println("");
+			System.out.println("");					//in outer loop to start a new line for output
 		}
 	}
 }
